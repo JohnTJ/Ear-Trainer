d83574fc17d568ef601e8dfb77b1e709a679d88b
JohnTJ <JohnTJ@users.noreply.github.com>
2019-08-03 20:24:37 -0400

    Initial Commit

---
+++ Ear Trainer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ear Trainer
+//
+//  Created by John Jones on 8/3/19.
+//  Copyright © 2019 John Jones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
